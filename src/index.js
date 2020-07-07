f06f2d92c56f5719e4f7895e3dd68c8b99f06296
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-07-08 01:53:00 +1000

    modify functions
    modify functions of play

--- src/index.js
+++ src/index.js
@@ -6,21 +6,51 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+      className="square" 
+      onClick={()=> this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor(props){
+    super(props);
+    this.state={
+      squares: Array(9).fill(null),
+      xIsNest:true,
+    };
   }
-
+  //handleClick function
+  handleClick(i){
+    //create a new copy of squares array which can be modified.
+    const squares=this.state.squares.slice();
+    squares[i]=this.state.xIsNest ? 'X' : '0';
+    this.setState({
+      squares:squares,
+      xIsNest:!this.state.xIsNest,
+      });
+  }
+  renderSquare(i) {
+    return (
+    <Square 
+    value={this.state.squares[i]}
+    onClick={()=>this.handleClick(i)}
+    />
+    );
+  }
   render() {
-    const status = 'Next player: X';
-
+    const winner=calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status='Winner:'+winner;
+    }
+    else{
+      status = 'Next player: '+ (this.state.xIsNest ? 'X': '0');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -66,3 +96,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
